ca0cbbd30532c176080accbb59f02ca0fe995998
abSWU <andrewbarsoum8@gmail.com>
2026-09-01 19:50:43 -0400

    created sql db and tables

---
+++ database/schema.sql
@@ -0,0 +1,28 @@
+-- Create our database
+CREATE DATABASE COP4331;
+USE COP4331;
+
+-- Create our tables for our DB
+-- First, lets create our user table
+
+CREATE TABLE Users(
+    ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, -- Unique ID for each user
+    FirstName VARCHAR(50) NOT NULL DEFAULT '',
+    LastName VARCHAR(50) NOT NULL DEFAULT '',
+    Username VARCHAR(50) NOT NULL DEFAULT '',
+    Password VARCHAR(50) NOT NULL DEFAULT ''
+)ENGINE = InnoDB;
+
+-- Now lets create the contacts table
+CREATE TABLE Contacts(
+    ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    FirstName VARCHAR(50) NOT NULL DEFAULT '',
+    LastName VARCHAR(50) NOT NULL DEFAULT '',
+    Phone VARCHAR(50) NOT NULL DEFAULT '',
+    Email VARCHAR(50) NOT NULL DEFAULT '',
+    UserID INT NOT NULL, -- used to tell us which contacts belong to which users
+
+    FOREIGN KEY (UserID) REFERENCES Users(ID)
+
+)ENGINE = InnoDB;
+
