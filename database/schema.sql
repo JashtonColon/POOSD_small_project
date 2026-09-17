ad11a13bee2f66cc22b34370925691867506feb9
abSWU <andrewbarsoum8@gmail.com>
2026-09-17 15:06:20 -0400

    removed defaults, added date, user unique

--- database/schema.sql
+++ database/schema.sql
@@ -6,20 +6,21 @@ USE COP4331;
 -- First, lets create our user table
 
 CREATE TABLE Users(
-    ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, -- Unique ID for each user
-    FirstName VARCHAR(50) NOT NULL DEFAULT '',
-    LastName VARCHAR(50) NOT NULL DEFAULT '',
-    Username VARCHAR(50) NOT NULL DEFAULT '',
+    ID INT PRIMARY KEY AUTO_INCREMENT, -- Unique ID for each user
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Username VARCHAR(50) NOT NULL UNIQUE,
+    RegDate DATE NOT NULL DEFAULT (CURRENT_DATE()), -- Default value is the current time when they register
     Password VARCHAR(50) NOT NULL DEFAULT ''
 )ENGINE = InnoDB;
 
 -- Now lets create the contacts table
 CREATE TABLE Contacts(
     ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    FirstName VARCHAR(50) NOT NULL DEFAULT '',
-    LastName VARCHAR(50) NOT NULL DEFAULT '',
-    Phone VARCHAR(50) NOT NULL DEFAULT '',
-    Email VARCHAR(50) NOT NULL DEFAULT '',
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Phone VARCHAR(50) NOT NULL,
+    Email VARCHAR(50) NOT NULL,
     UserID INT NOT NULL, -- used to tell us which contacts belong to which users
 
     FOREIGN KEY (UserID) REFERENCES Users(ID)
