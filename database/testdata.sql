134fc1b75d5845bacd8917ec7ac05b10bdc4324a
LightningNick2003 <Killanick2003@gmail.com>
2026-09-20 16:14:42 -0400

    Improve database test data

--- database/testdata.sql
+++ database/testdata.sql
@@ -1,37 +1,56 @@
--- Test data to populate our currently empty sql tables
-
+-- Development test data for fresh, empty tables only
+--Test password for all users is "TestPassword123!"
+USE COP4331;
 -- Inserting some users
 INSERT INTO Users(FirstName, LastName, Username, Password)
-VALUES('John', 'Doe', 'myusername', 'mypassword');
+VALUES('John', 'Doe', 'myusername', '$2y$12$8dEbZqVy/wwyR/UiD6MeCu1cLRNM5IuRDeZ5/v5PADUO2dfRiGoyu');
 
 INSERT INTO Users(FirstName, LastName, Username, Password)
-VALUES('Jane', 'Doe', 'janedoe1', 'abcd');
+VALUES('Jane', 'Doe', 'janedoe1', '$2y$12$OCLo31FwS.mGu4QWtvSobuPSr0uWxmH01Oyq6mpUAoBh78svigfya');
 
 INSERT INTO Users(FirstName, LastName, Username, Password)
-VALUES('Jimmy', 'Goober', 'ilikehats', 'pleasedonthackme');
+VALUES('Jimmy', 'Goober', 'ilikehats', '$2y$12$YCdM5A8uYs1eVwnh6Vtsk.N4LUGoln2csl33vaUbshvSwxIulBHhm');
 
 -- Insert some contacts
 
 INSERT INTO Contacts(FirstName, LastName, Phone, Email, UserID)
-VALUES ('John', 'Smith', '(123)456-7890', 'jsmith@email.com', '1');
+SELECT 'John', 'Smith', '(123)456-7890', 'jsmith@email.com', ID
+FROM Users
+WHERE Username = 'myusername';
 
 INSERT INTO Contacts(FirstName, LastName, Phone, Email, UserID)
-VALUES ('Jim', 'Pop', '(941)456-7890', 'jpop@email.com', '2');
+SELECT 'Jordan', 'Jones', '(941)456-7890', 'jordan.jones@email.com', ID
+FROM Users
+WHERE Username = 'myusername';
 
 INSERT INTO Contacts(FirstName, LastName, Phone, Email, UserID)
-VALUES ('Hol', 'Low', '(941)456-0010', 'hlow@email.com', '2');
+SELECT 'Sarah', 'Jobs', '(941)456-0010', 'sarah.jobs@email.com', ID
+FROM Users
+WHERE Username = 'myusername';
+
+--This contact belongs to Jane
 
 INSERT INTO Contacts(FirstName, LastName, Phone, Email, UserID)
-VALUES ('Aura', 'Monster', '(123)456-6767', 'nsubaru@email.com', '3');
+SELECT 'Jim', 'Pop', '(123)456-6767', 'jpop@email.com', ID
+FROM Users
+WHERE Username = 'janedoe1';
+
+--These contacts belong to Jimmy
 
 INSERT INTO Contacts(FirstName, LastName, Phone, Email, UserID)
-VALUES ('Tim', 'Kneearaise', '(123)456-3261', 'traise@email.com', '3');
+SELECT 'Tim', 'Kneearaise', '(123)456-3261', 'traise@email.com', ID
+FROM Users
+WHERE Username = 'ilikehats';
 
 INSERT INTO Contacts(FirstName, LastName, Phone, Email, UserID)
-VALUES ('Who', 'Areyou', '(123)050-0707', 'who@email.com', '3');
+SELECT 'Who', 'Areyou', '(123)050-0707', 'who@email.com', ID
+FROM Users
+WHERE Username = 'ilikehats';
 
-SELECT *
-FROM Users;
+SELECT ID, FirstName, LastName, Username, RegDate
+FROM Users
+ORDER BY ID;
 
-SELECT *
-FROM Contacts;
+SELECT ID, FirstName, LastName, Phone, Email, UserID, DateCreated
+FROM Contacts
+ORDER BY UserID, LastName, FirstName;
